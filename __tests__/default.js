a64807d6405ec7462613d79efaaebcc03d1f05f2
ethancfchen <ethan.cfchen@gmail.com>
2017-09-27 02:08:12 +0800

    Add first test

--- __tests__/default.js
+++ __tests__/default.js
@@ -1,5 +1,15 @@
+const gulp = require('gulp');
+const loadAllTasks = require('../index');
+
 describe('gulp-load-all-tasks', () => {
-  test('TODO', () => {
-    require('../index')();
+  beforeEach(() => {
+    loadAllTasks();
+  });
+
+  describe('use default options', () => {
+    it('shoud load dummy task', () => {
+      const tasks = gulp.tree().nodes;
+      expect(tasks.includes('dummy')).toBeTruthy();
+    });
   });
 });
